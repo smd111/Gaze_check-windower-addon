fe82c09bfef5d7700059bc0e3120ea265efd0976
KenshiDRK <jaciarriaga@ono.com>
2016-05-14 02:45:13 +0200

    New gaze additions and features
    New gazes added and it now will turn too on gazes that apply even if the
    mob is not facing you but you are facing the mob (good for melee DDs),
    for this change I needed to change the way it turns from using the
    targets facing to the getAngle function, this change also solves a bug
    that happens if you lose hate in middle of a gaze attack, resulting in
    not turning back to the mob.

--- Gaze_check.lua
+++ Gaze_check.lua
@@ -19,10 +19,10 @@ gaze_attacks = {[284]="Cold Stare",[292]="Blank Gaze",[370]="Baleful Gaze",[386]
 [502]="Mortal Ray",[550]="Hypnosis",[551]="Mind Break",[577]="Jettatura",[586]="Blank Gaze",[589]="Mortal Ray",[648]="Petro Eyes",[653]="Chaotic Eye",
 [785]="Light of Penance",[1111]="Numbing Glare",[1113]="Tormentful Glare",[1115]="Torpid Glare",[1138]="Hypnosis",[1139]="Mind Break",[1174]="Petro Eyes",
 [1184]="Petro Eyes",[1322]="Gerjis' Grip",[1359]="Chthonian Ray",[1360]="Apocalyptic Ray",[1563]="Cold Stare",[1603]="Baleful Gaze",[1680]="Predatory Glare",
-[1713]="Yawn",[1759]="Hypnotic Sway",[1762]="Belly Dance",[1862]="Awful Eye",[1883]="Mortal Ray",[1950]="Belly Dance",[2111]="Eternal Damnation",
-[2155]="Torpefying Charge",[2209]="Blink of Peril",[2424]="Terror Eye",[2570]="Afflicting Gaze",[2534]="Minax Glare",[3760]="Beguiling Gaze",
-[2768]="Deathly Glare",[2814]="Yawn",[2828]="Jettatura",[3031]="Sylvan Slumber",[3032]="Crushing Gaze",[3358]="Blank Gaze",[3898]="Chaotic Eye",
-[3916]="Jettatura",[4036]="Mortal Ray",}
+[1694]="Vile Belch",[1695]="Hypnic Lamp",[1713]="Yawn",[1716]="Frigid Shuffle",[1759]="Hypnotic Sway",[1762]="Belly Dance",[1862]="Awful Eye",[1883]="Mortal Ray",
+[1950]="Belly Dance",[1978]="Abominable Belch",[2111]="Eternal Damnation",[2155]="Torpefying Charge",[2209]="Blink of Peril",[2424]="Terror Eye",[2466]="Washtub",
+[2570]="Afflicting Gaze",[2534]="Minax Glare",[2610]="Vacant Gaze",[2768]="Deathly Glare",[2814]="Yawn",[2817]="Frigid Shuffle",[2828]="Jettatura",
+[3031]="Sylvan Slumber",[3032]="Crushing Gaze",[3358]="Blank Gaze",[3760]="Beguiling Gaze",[3898]="Chaotic Eye",[3916]="Jettatura",[4036]="Mortal Ray",}
 
 perm_gaze_attacks = {[2156]="Grim Glower",[2392]="Oppressive Glare",[2776]="Shah Mat",}
 perm_gaze_control = {["Peiste"]={skills=T{2156, 2392},delay=3,ender=T{4}},["Caturae"]={skills=T{2776},delay=6,ender=T{4,6}},}
@@ -59,14 +59,20 @@ function check_target_id(packet) --checks to see if player is one of the targets
     end
     return false
 end
-function check_facing(index)
-    local actor = windower.ffxi.get_mob_by_id(index)
+function check_facing(packet)
+    local actor = windower.ffxi.get_mob_by_id(packet['Actor'])
     local player = windower.ffxi.get_mob_by_target('me')
     local dir = {actor=(V{player.x, player.y} - V{actor.x, actor.y}),player=(V{actor.x, actor.y} - V{player.x, player.y})}
     local heading = {actor=(V{}.from_radian(actor.facing)),player=(V{}.from_radian(player.facing))}
     local angle = {actor=(V{}.angle(dir.actor, heading.actor):degree():abs()),player=(V{}.angle(dir.player, heading.player):degree():abs())}
-    if angle.player < 90 and angle.actor < 90 then
-        return true
+    for i,v in pairs(packet) do
+        if angle.player < 90 and angle.actor < 90 then
+            return true
+        elseif string.match(i, 'Target %d+ Action %d+ Param') then --Turn on gazes than don't need the mob to face you to apply
+            if T{1694, 1695, 1713, 1716, 1762, 1950, 1978, 2155, 2814, 2817}:contains(v) and angle.player < 90 then
+                return true
+            end
+        end
     end
     return false
 end
@@ -104,22 +110,22 @@ windower.register_event('incoming chunk', function(id, data, modified, injected,
                 perm_gaze = false
                 perm_trigered_actor = 0
                 mob_type = ""
-                windower.ffxi.turn:schedule(1,windower.ffxi.get_mob_by_target('t').facing+math.pi)
+                windower.ffxi.turn:schedule(1,(getAngle()+180):radian())
             end
         end
     end
     if id == 0x028 then
         local packet = packets.parse('incoming', data)
         if windower.ffxi.get_player().in_combat and windower.ffxi.get_mob_by_target('t') then
-            if packet['Category'] == 7 and not pet_check(packet['Actor']) and (check_target_id(packet) or check_facing(packet['Actor'])) and check_target_action(packet) then
+            if packet['Category'] == 7 and not pet_check(packet['Actor']) and (check_target_id(packet) or check_facing(packet)) and check_target_action(packet) then
                 gaze = true
                 trigered_actor = packet['Actor']
-                windower.ffxi.turn(windower.ffxi.get_mob_by_id(packet['Actor']).facing) 
+                windower.ffxi.turn((getAngle()+180):radian()+math.pi)
             elseif packet['Category'] == 11 and packet['Actor'] == trigered_actor and gaze then
                 if settings.gaze_watch and gaze_attacks[packet['Param']] then
                     gaze = false
                     if not perm_gaze then
-                        windower.ffxi.turn:schedule(1,windower.ffxi.get_mob_by_target('t').facing+math.pi)
+                        windower.ffxi.turn:schedule(1,(getAngle()+180):radian())
                     end
                 elseif settings.perm_gaze_watch and perm_gaze_attacks[packet['Param']] then
                     perm_trigered_actor = windower.ffxi.get_mob_by_id(packet['Actor']).index
