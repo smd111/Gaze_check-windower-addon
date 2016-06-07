9a5c9a3485e7773585c9388cc19b57c3238ce6c4
KenshiDRK <jaciarriaga@ono.com>
2016-06-07 09:57:23 +0200

    Update for 7 June 2016 and improvements
    Update for June 7, check_facing function now check if the action is done
    to a pt member or pet to avoid turning on mobs from another pt if you
    are facing them, fixed a possible bug (a peiste doing torpefying charge
    while glow eyes are up caused gaze to go false making it turn on
    auto_point, it also saves a line), also added the actor to getAngle to
    turn based on the actor and not the target.

--- Gaze_check.lua
+++ Gaze_check.lua
@@ -2,7 +2,7 @@ _addon.name = 'Gaze_check'
 _addon.author = 'smd111/Kenshi'
 _addon.command = 'gazecheck'
 _addon.commands = {'gzc'}
-_addon.version = '2.02'
+_addon.version = '2.03'
 
 require 'luau'
 require('vectors')
@@ -19,7 +19,7 @@ gaze_attacks = {[284]="Cold Stare",[292]="Blank Gaze",[370]="Baleful Gaze",[386]
 [1694]="Vile Belch",[1695]="Hypnic Lamp",[1713]="Yawn",[1716]="Frigid Shuffle",[1759]="Hypnotic Sway",[1762]="Belly Dance",[1862]="Awful Eye",[1883]="Mortal Ray",
 [1950]="Belly Dance",[1978]="Abominable Belch",[2111]="Eternal Damnation",[2155]="Torpefying Charge",[2209]="Blink of Peril",[2424]="Terror Eye",[2466]="Washtub",
 [2570]="Afflicting Gaze",[2534]="Minax Glare",[2610]="Vacant Gaze",[2768]="Deathly Glare",[2814]="Yawn",[2817]="Frigid Shuffle",[2828]="Jettatura",
-[3031]="Sylvan Slumber",[3032]="Crushing Gaze",[3358]="Blank Gaze",[3760]="Beguiling Gaze",[3898]="Chaotic Eye",[3916]="Jettatura",[4036]="Mortal Ray",}
+[3031]="Sylvan Slumber",[3032]="Crushing Gaze",[3358]="Blank Gaze",[3760]="Beguiling Gaze",[3898]="Chaotic Eye",[3916]="Jettatura"}
 
 perm_gaze_attacks = {[2156]="Grim Glower",[2392]="Oppressive Glare",[2776]="Shah Mat",}
 perm_gaze_control = {["Peiste"]={skills=T{2156, 2392},delay=3,ender=T{4}},["Caturae"]={skills=T{2776},delay=6,ender=T{4,6}},}
@@ -53,17 +53,29 @@ function check_target_id(packet) --checks to see if player is one of the targets
     return false
 end
 function check_facing(packet)
+    local key_indices = {'p0', 'p1','p2','p3','p4','p5','a10','a11','a12','a13','a14','a15','a20','a21','a22','a23','a24','a25'}
+    local party = windower.ffxi.get_party()
     local actor = windower.ffxi.get_mob_by_id(packet['Actor'])
     local player = windower.ffxi.get_mob_by_target('me')
     local dir = {actor=(V{player.x, player.y} - V{actor.x, actor.y}),player=(V{actor.x, actor.y} - V{player.x, player.y})}
     local heading = {actor=(V{}.from_radian(actor.facing)),player=(V{}.from_radian(player.facing))}
     local angle = {actor=(V{}.angle(dir.actor, heading.actor):degree():abs()),player=(V{}.angle(dir.player, heading.player):degree():abs())}
     for i,v in pairs(packet) do
-        if angle.player < 90 and angle.actor < 90 then
-            return true
-        elseif string.match(i, 'Target %d+ Action %d+ Param') then --Turn on gazes than don't need the mob to face you to apply
-            if T{1694, 1695, 1713, 1716, 1762, 1950, 1978, 2155, 2814, 2817}:contains(v) and angle.player < 90 then
-                return true
+        if string.match(i, 'Target %d+ ID') then
+            local index = windower.ffxi.get_mob_by_id(v).index
+            for k = 1, 18 do
+                local member = party[key_indices[k]]
+                if member and member.mob and (member.mob.id == v or member.mob.pet_index == index) or actor.id == v then
+                    for ind, val in pairs(packet) do  
+                    if angle.player < 90 and angle.actor < 90 then
+                        return true     
+                    elseif string.match(ind, 'Target %d+ Action %d+ Param') then --Turn on gazes than don't need the mob to face you to apply
+                        if T{1694, 1695, 1713, 1716, 1762, 1950, 1978, 2155, 2814, 2817}:contains(val) and angle.player < 90 then
+                            return true
+                        end
+                    end
+                    end
+                end
             end
         end
     end
@@ -112,13 +124,11 @@ windower.register_event('incoming chunk', function(id, data, modified, injected,
             if packet['Category'] == 7 and not pet_check(packet['Actor']) and (check_target_id(packet) or check_facing(packet)) and check_target_action(packet) then
                 gaze = true
                 trigered_actor = packet['Actor']
-                windower.ffxi.turn((getAngle()+180):radian()+math.pi)
+                windower.ffxi.turn((getAngle(packet['Actor'])+180):radian()+math.pi)
             elseif packet['Category'] == 11 and packet['Actor'] == trigered_actor and gaze then
-                if settings.auto_gaze and gaze_attacks[packet['Param']] then
+                if settings.auto_gaze and gaze_attacks[packet['Param']] and not perm_gaze then
                     gaze = false
-                    if not perm_gaze then
-                        windower.ffxi.turn:schedule(1,(getAngle()+180):radian())
-                    end
+                    windower.ffxi.turn:schedule(1,(getAngle()+180):radian())
                 elseif settings.auto_perm_gaze and perm_gaze_attacks[packet['Param']] then
                     perm_trigered_actor = windower.ffxi.get_mob_by_id(packet['Actor']).index
                 end
@@ -127,9 +137,9 @@ windower.register_event('incoming chunk', function(id, data, modified, injected,
         end
     end
 end)
-function getAngle()
+function getAngle(index)
     local P = windower.ffxi.get_mob_by_target('me') --get player
-    local M = windower.ffxi.get_mob_by_target('t') --get target
+    local M = index and windower.ffxi.get_mob_by_id(index) or windower.ffxi.get_mob_by_target('t') --get target
     local delta = {Y = (P.y - M.y),X = (P.x - M.x)} --subtracts target pos from player pos
     local angleInDegrees = (math.atan2( delta.Y, delta.X) * 180 / math.pi)*-1 
     local mult = 10^0
